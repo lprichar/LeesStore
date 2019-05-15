b2c3fc99943e7c3e2de2fb966c7cad1734194081
Lee Richardson <github@leerichardson.com>
2019-05-14 23:21:53 -0400

    back-end crud

---
+++ aspnet-core/src/LeesStore.Core/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Domain.Entities;
+
+namespace LeesStore.Products
+{
+    public class Product : Entity<int>
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
