0b1b7dcc219d9cfb10265ae9db085191a068aa38
Lee Richardson <github@leerichardson.com>
2019-07-23 11:20:26 -0400

    back-end controller for a hello world Excel

---
+++ aspnet-core/src/LeesStore.Web.Core/Controllers/ProductFilesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace LeesStore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductFilesController : AbpController
+    {
+        [HttpPost]
+        [Route("{filename}.xlsx")]
+        public async Task<ActionResult> Download(string fileName)
+        {
+            await Task.Yield();
+            var fileMemoryStream = GenerateReportAndWriteToMemoryStream();
+            return File(fileMemoryStream,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                fileName + ".xlsx");
+        }
+
+        private byte[] GenerateReportAndWriteToMemoryStream()
+        {
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Data");
+                worksheet.Cells[1, 1].Value = "Hello World";
+                return package.GetAsByteArray();
+            }
+        }
+    }
+}
