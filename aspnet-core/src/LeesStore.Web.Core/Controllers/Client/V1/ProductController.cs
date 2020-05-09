ffa54864bbfe181439e5a84e760e4c4a7d5df794
Lee Richardson <github@leerichardson.com>
2020-05-09 14:20:10 -0400

    Add a client API

---
+++ aspnet-core/src/LeesStore.Web.Core/Controllers/Client/V1/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Domain.Repositories;
+using LeesStore.Products;
+using LeesStore.Products.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LeesStore.Controllers.Client.V1
+{
+    //[DontWrapResult(WrapOnError = false, WrapOnSuccess = false, LogError = true)]
+    public class ProductController : LeesStoreControllerBase
+    {
+        private readonly IRepository<Product, int> _productRepository;
+
+        public ProductController(IRepository<Product, int> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet("api/client/v1/product/{id}")]
+        //[ProducesResponseType(typeof(List<Product>, 200))]
+        public async Task<ProductDto> Get(int id)
+        {
+            var product = await _productRepository.GetAsync(id);
+            var productDto = ObjectMapper.Map<ProductDto>(product);
+            return productDto;
+        }
+    }
+}
