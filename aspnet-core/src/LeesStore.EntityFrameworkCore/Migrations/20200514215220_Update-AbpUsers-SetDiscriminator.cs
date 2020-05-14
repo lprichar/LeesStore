98ae278851296e405097d4cf4392d2e12c0bcfa4
Lee Richardson <github@leerichardson.com>
2020-05-14 18:31:31 -0400

    Set discriminator

---
+++ aspnet-core/src/LeesStore.EntityFrameworkCore/Migrations/20200514215220_Update-AbpUsers-SetDiscriminator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeesStore.Migrations
+{
+    public partial class UpdateAbpUsersSetDiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE AbpUsers SET Discriminator = 'User'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
