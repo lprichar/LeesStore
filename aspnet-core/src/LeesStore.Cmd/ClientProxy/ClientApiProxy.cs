3ebfde7c00b93d2d34069059562b746ed7cad0c9
Lee Richardson <github@leerichardson.com>
2020-05-17 08:16:32 -0400

    Add a client token authenticate controller

--- aspnet-core/src/LeesStore.Cmd/ClientProxy/ClientApiProxy.cs
+++ aspnet-core/src/LeesStore.Cmd/ClientProxy/ClientApiProxy.cs
@@ -46,6 +46,76 @@ namespace LeesStore.Cmd.ClientProxy
     
         /// <returns>Success</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<AuthenticateResultModel> AuthenticateAsync(ClientAuthenticateModel body)
+        {
+            return AuthenticateAsync(body, System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<AuthenticateResultModel> AuthenticateAsync(ClientAuthenticateModel body, System.Threading.CancellationToken cancellationToken)
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/client/v1/tokenauth");
+    
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    var content_ = new System.Net.Http.StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(body, _settings.Value));
+                    content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json-patch+json");
+                    request_.Content = content_;
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200") 
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<AuthenticateResultModel>(response_, headers_).ConfigureAwait(false);
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false); 
+                            throw new ApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+            
+                        return default(AuthenticateResultModel);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+    
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
         public System.Threading.Tasks.Task<ProductDto> GetProductAsync(int id)
         {
             return GetProductAsync(id, System.Threading.CancellationToken.None);
@@ -102,6 +172,69 @@ namespace LeesStore.Cmd.ClientProxy
                         }
             
                         return default(ProductDto);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+    
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task HelloAsync()
+        {
+            return HelloAsync(System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task HelloAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/client/v1/hello");
+    
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200") 
+                        {
+                            return;
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false); 
+                            throw new ApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
                     }
                     finally
                     {
@@ -208,6 +341,38 @@ namespace LeesStore.Cmd.ClientProxy
         }
     }
 
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.21.0 (Newtonsoft.Json v12.0.0.0)")]
+    public partial class ClientAuthenticateModel 
+    {
+        [Newtonsoft.Json.JsonProperty("apiKey", Required = Newtonsoft.Json.Required.AllowNull)]
+        [System.ComponentModel.DataAnnotations.StringLength(256)]
+        public string ApiKey { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("secret", Required = Newtonsoft.Json.Required.AllowNull)]
+        [System.ComponentModel.DataAnnotations.StringLength(32)]
+        public string Secret { get; set; }
+    
+    
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.21.0 (Newtonsoft.Json v12.0.0.0)")]
+    public partial class AuthenticateResultModel 
+    {
+        [Newtonsoft.Json.JsonProperty("accessToken", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string AccessToken { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("encryptedAccessToken", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string EncryptedAccessToken { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("expireInSeconds", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public int ExpireInSeconds { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("userId", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public long UserId { get; set; }
+    
+    
+    }
+    
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.21.0 (Newtonsoft.Json v12.0.0.0)")]
     public partial class ProductDto 
     {
---
+++ aspnet-core/src/LeesStore.Web.Core/Controllers/Client/V1/ClientTokenAuthController.cs
@@ -0,0 +1,171 @@
+﻿using Abp.Authorization;
+using Abp.Authorization.Users;
+using Abp.MultiTenancy;
+using Abp.Runtime.Security;
+using Abp.Runtime.Session;
+using Abp.Web.Models;
+using LeesStore.Authentication.JwtBearer;
+using LeesStore.Authorization;
+using LeesStore.Authorization.Users;
+using LeesStore.Models.TokenAuth;
+using LeesStore.MultiTenancy;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace LeesStore.Controllers.Client.V1
+{
+    public class ClientAuthenticateModel
+    {
+        [Required]
+        [StringLength(AbpUserBase.MaxEmailAddressLength)]
+        public string ApiKey { get; set; }
+
+        [Required]
+        [StringLength(AbpUserBase.MaxPlainPasswordLength)]
+        public string Secret { get; set; }
+    }
+
+    public class ClientTokenAuthController : LeesStoreControllerBase
+    {
+        private readonly IApiKeyAuthenticationService _apiKeyAuthenticationService;
+        private readonly IAbpSession _session;
+        private readonly ITenantCache _tenantCache;
+        private readonly LogInManager _logInManager;
+        private readonly TokenAuthConfiguration _configuration;
+        private readonly AbpLoginResultTypeHelper _abpLoginResultTypeHelper;
+
+        public ClientTokenAuthController(
+            IApiKeyAuthenticationService apiKeyAuthenticationService,
+            IAbpSession session,
+            ITenantCache tenantCache,
+            LogInManager logInManager,
+            TokenAuthConfiguration configuration,
+            AbpLoginResultTypeHelper abpLoginResultTypeHelper)
+        {
+            _apiKeyAuthenticationService = apiKeyAuthenticationService;
+            _session = session;
+            _tenantCache = tenantCache;
+            _logInManager = logInManager;
+            _configuration = configuration;
+            _abpLoginResultTypeHelper = abpLoginResultTypeHelper;
+        }
+
+        /// <summary>
+        /// Authenticates an API Key and Secret.  If successful AuthenticateResultModel will contain a token that
+        /// should be passed to subsequent methods in the header as a Bearer Auth token.
+        /// </summary>
+        /// <param name="model">Contains the API Key and Secret</param>
+        /// <returns>The authentication results</returns>
+        [HttpPost("api/client/v1/tokenauth", Name = nameof(Authenticate))]
+        [ProducesResponseType(typeof(AuthenticateResultModel), 200)]
+        [DontWrapResult(WrapOnError = false, WrapOnSuccess = false, LogError = true)]
+        public async Task<IActionResult> Authenticate([FromBody] ClientAuthenticateModel model)
+        {
+            /*
+             * This 1st Authenticate() looks only in ApiKeys, which are assumed to be unique across Tenants (unlike Users),
+             * thus we can pull back a TenantId on success and set the session to use it
+             */
+            var apiKeyAuthenticationResult = await _apiKeyAuthenticationService.Authenticate(model.ApiKey, model.Secret);
+
+            if (!apiKeyAuthenticationResult.Success)
+            {
+                // this 401 is much cleaner than what the regular TokenAuthController returns.  It does a HttpFriendlyException which results in 500 :|
+                return new UnauthorizedObjectResult(null);
+            }
+
+            using (_session.Use(apiKeyAuthenticationResult.TenantId, null))
+            {
+                /*
+                 * This 2nd Authenticate is almost entirely guaranteed to succeed except for a few edge cases like if the
+                 * tenant is inactive.  However, it's necessary in order to get a loginResult and create an access token.
+                 */
+                AbpLoginResult<Tenant, User> loginResult = await GetLoginResultAsync(
+                    model.ApiKey,
+                    model.Secret,
+                    GetTenancyNameOrNull()
+                );
+
+                return new OkObjectResult(CreateAccessToken(loginResult));
+            }
+        }
+
+        private async Task<AbpLoginResult<Tenant, User>> GetLoginResultAsync(string usernameOrEmailAddress, string password, string tenancyName)
+        {
+            var loginResult = await _logInManager.LoginAsync(usernameOrEmailAddress, password, tenancyName);
+
+            if (loginResult.Result != AbpLoginResultType.Success)
+            {
+                throw _abpLoginResultTypeHelper.CreateExceptionForFailedLoginAttempt(loginResult.Result,
+                    usernameOrEmailAddress, tenancyName);
+            }
+
+            return loginResult;
+        }
+
+        private string GetTenancyNameOrNull()
+        {
+            if (!_session.TenantId.HasValue)
+            {
+                return null;
+            }
+
+            return _tenantCache.GetOrNull(_session.TenantId.Value)?.TenancyName;
+        }
+
+        private string CreateAccessToken(IEnumerable<Claim> claims, TimeSpan? expiration = null)
+        {
+            var now = DateTime.UtcNow;
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _configuration.Issuer,
+                audience: _configuration.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(expiration ?? _configuration.Expiration),
+                signingCredentials: _configuration.SigningCredentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        }
+
+        private AuthenticateResultModel CreateAccessToken(AbpLoginResult<Tenant, User> loginResult)
+        {
+            var accessToken = CreateAccessToken(CreateJwtClaims(loginResult.Identity));
+
+            return new AuthenticateResultModel
+            {
+                AccessToken = accessToken,
+                EncryptedAccessToken = GetEncrpyedAccessToken(accessToken),
+                ExpireInSeconds = (int)_configuration.Expiration.TotalSeconds,
+                UserId = loginResult.User.Id
+            };
+        }
+
+        private List<Claim> CreateJwtClaims(ClaimsIdentity identity)
+        {
+            var claims = identity.Claims.ToList();
+            var nameIdClaim = claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+
+            // Specifically add the jti (random nonce), iat (issued timestamp), and sub (subject/user) claims.
+            claims.AddRange(new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, nameIdClaim.Value),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.Now.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            });
+
+            return claims;
+        }
+
+        private string GetEncrpyedAccessToken(string accessToken)
+        {
+            return SimpleStringCipher.Instance.Encrypt(accessToken, AppConsts.DefaultPassPhrase);
+        }
+    }
+}
