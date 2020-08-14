61eb8e42fbd40733829448543e8299b3e8236dc5
Lee Richardson <github@leerichardson.com>
2020-08-13 22:42:07 -0400

    Create an app service, set firewall rule for sql

--- scripts/create-environment.ps1
+++ scripts/create-environment.ps1
@@ -12,7 +12,16 @@ $sqlName = "leesstorequickdeploy"
 az sql server create -g $resourceGroupName -n $sqlName `
     -u $sqlUsername -p $sqlPassword -l $location
 
+# add firewall rule for access from Azure services
+az sql server firewall-rule create -g $resourceGroupName -s $sqlName -n Azure --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
 # create a sql database
 $sqlDbName = "LeesStoreQuickDeploy"
 az sql db create -g $resourceGroupName -s $sqlName -n $sqlDbName `
     --compute-model "Serverless" -e "GeneralPurpose" -f "Gen5" -c 1 --max-size "1GB"
+
+# create an app service
+$appServicePlan = "LeesStoreServicePlan"
+$webAppName = "leesstore2"
+az appservice plan create -n $appServicePlan -g $resourceGroupName --sku Free -l $location
+az webapp create -n $webAppName -g $resourceGroupName -p $appServicePlan
